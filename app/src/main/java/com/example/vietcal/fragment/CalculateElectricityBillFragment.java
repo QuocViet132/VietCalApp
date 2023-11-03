29bacdb96fa33fccf0540c711775b17442f7896f
QuocVietDev <nguyenquocviet2001132@gmail.com>
2023-11-04 00:28:01 +0700

    Complete Extension Electricity Bill

--- app/src/main/java/com/example/vietcal/fragment/CalculateElectricityBillFragment.java
+++ app/src/main/java/com/example/vietcal/fragment/CalculateElectricityBillFragment.java
@@ -1,66 +1,164 @@
 package com.example.vietcal.fragment;
 
+import android.icu.text.DecimalFormat;
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.vietcal.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalculateElectricityBillFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Locale;
+
 public class CalculateElectricityBillFragment extends Fragment {
+    private View view;
+    private EditText etStartNumber;
+    private EditText etEndNumber;
+    private EditText etTotalNumber;
+    private TextView tvResultAmountBeforeTax;
+    private TextView tvResultAmountTax;
+    private TextView tvResultAmountTotal;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private Button btnCalculate;
+    private Button btnDelete;
+    private int startNumber = 0;
+    private int endNumber = 0;
+    private int totalNumber = 0;
+    private int amountBeforeTax = 0;
+    private Double amountTax = 0.0;
+    private Double amountTotal = 0.0;
+    private final Locale sk = Locale.forLanguageTag("sk-SK");
+    private final NumberFormat numberFormat = NumberFormat.getInstance(sk);
     public CalculateElectricityBillFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalculateElectricityBillFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CalculateElectricityBillFragment newInstance(String param1, String param2) {
-        CalculateElectricityBillFragment fragment = new CalculateElectricityBillFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculate_electricity_bill, container, false);
+        view = inflater.inflate(R.layout.fragment_calculate_electricity_bill, container, false);
+        etStartNumber = view.findViewById(R.id.et_start_number);
+        etEndNumber = view.findViewById(R.id.et_end_number);
+        etTotalNumber = view.findViewById(R.id.et_total_number);
+        tvResultAmountBeforeTax = view.findViewById(R.id.tv_result_amount_before_tax);
+        tvResultAmountTax = view.findViewById(R.id.tv_result_amount_tax);
+        tvResultAmountTotal = view.findViewById(R.id.tv_result_amount_total);
+        btnCalculate = view.findViewById(R.id.btn_calculate_electricity);
+        btnDelete = view.findViewById(R.id.btn_delete_electricity);
+
+        clickListener();
+        return view;
+    }
+
+    private void clickListener() {
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!etStartNumber.getText().toString().isEmpty() && !etEndNumber.getText().toString().isEmpty()) {
+                    startNumber = Integer.parseInt(etStartNumber.getText().toString());
+                    endNumber = Integer.parseInt(etEndNumber.getText().toString());
+                    if(startNumber <= endNumber) {
+                        totalNumber = endNumber - startNumber;
+                        calculateTotalAmount();
+                    }else {
+                        Toast.makeText(view.getContext(),"Số cuối phải lớn hơn số đầu",Toast.LENGTH_SHORT).show();
+                    }
+                }else if(!etTotalNumber.getText().toString().isEmpty()) {
+                    totalNumber = Integer.parseInt(etTotalNumber.getText().toString());
+                    calculateTotalAmount();
+                }else {
+                    Toast.makeText(view.getContext(),"Hãy nhập đủ thông số",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etStartNumber.setText("");
+                etEndNumber.setText("");
+                etTotalNumber.setText("");
+                tvResultAmountBeforeTax.setText("");
+                tvResultAmountTax.setText("");
+                tvResultAmountTotal.setText("");
+            }
+        });
+    }
+
+    private int calculateElectricityBeforeTax(int mTotalNumber) {
+        int result = 0;
+        int amountLevel1 = 1728;
+        int amountLevel2 = 1786;
+        int amountLevel3 = 2074;
+        int amountLevel4 = 2612;
+        int amountLevel5 = 2919;
+        int amountLevel6 = 3015;
+
+        switch (checkAmountLevel(mTotalNumber)) {
+            case 1:
+                result = amountLevel1*mTotalNumber;
+                break;
+            case 2:
+                result = amountLevel1*50 + amountLevel2*(mTotalNumber-50);
+                break;
+            case 3:
+                result = amountLevel1*50 + amountLevel2*50 + amountLevel3*(mTotalNumber-100);
+                break;
+            case 4:
+                result = amountLevel1*50 + amountLevel2*50 + amountLevel3*100 + amountLevel4*(mTotalNumber-200);
+                break;
+            case 5:
+                result = amountLevel1*50 + amountLevel2*50 + amountLevel3*100 + amountLevel4*100 + amountLevel5*(mTotalNumber-300);
+                break;
+            case 6:
+                result = amountLevel1*50 + amountLevel2*50 + amountLevel3*100 + amountLevel4*100 + amountLevel5*100 + amountLevel6*(mTotalNumber-400);
+                break;
+        }
+        return result;
+    }
+
+    private int checkAmountLevel(int mTotalNumber) {
+        int level = 0;
+        int count = 0;
+        for(int i=0; i<6; i++) {
+            if (count<2 && mTotalNumber>0) {
+                mTotalNumber -= 50;
+                level++;
+                count++;
+            }else if(count<6 && mTotalNumber>0) {
+                mTotalNumber -= 100;
+                level++;
+                count++;
+            }else {
+                break;
+            }
+        }
+        return level;
+    }
+
+    private void calculateTotalAmount() {
+        amountBeforeTax = calculateElectricityBeforeTax(totalNumber);
+        amountTax = amountBeforeTax * 0.08;
+        amountTotal = amountBeforeTax + amountTax;
+
+        tvResultAmountBeforeTax.setText(numberFormat.format(amountBeforeTax));
+        tvResultAmountTax.setText(numberFormat.format(Math.round(amountTax)));
+        tvResultAmountTotal.setText(numberFormat.format(Math.round(amountTotal)));
     }
 }
