4d16f59c98686109657c0fcc92d5c9e667a92407
QuocVietDev <nguyenquocviet2001132@gmail.com>
2023-11-07 14:51:00 +0700

    Complete Extension Convert Gross To Net

--- app/src/main/java/com/example/vietcal/fragment/CalculateWaterBillFragment.java
+++ app/src/main/java/com/example/vietcal/fragment/CalculateWaterBillFragment.java
@@ -1,5 +1,7 @@
 package com.example.vietcal.fragment;
 
+import android.icu.number.FormattedNumber;
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +9,121 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.vietcal.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalculateWaterBillFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Locale;
+
 public class CalculateWaterBillFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private View view;
+    private Button btnCalculate;
+    private Button btnDelete;
+    private EditText etStartNumber;
+    private EditText etEndNumber;
+    private EditText etTotalNumber;
+    private TextView tvResultAmountTotal;
+    private int startNumber;
+    private int endNumber;
+    private int totalNumber;
+    private Locale sk = Locale.forLanguageTag("sk-SK");
+    private NumberFormat numberFormat = NumberFormat.getInstance(sk);
     public CalculateWaterBillFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalculateWaterBillFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CalculateWaterBillFragment newInstance(String param1, String param2) {
-        CalculateWaterBillFragment fragment = new CalculateWaterBillFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculate_water_bill, container, false);
+        view = inflater.inflate(R.layout.fragment_calculate_water_bill, container, false);
+        btnCalculate = view.findViewById(R.id.btn_calculate_water);
+        btnDelete = view.findViewById(R.id.btn_delete_water);
+        etStartNumber = view.findViewById(R.id.et_start_number);
+        etEndNumber = view.findViewById(R.id.et_end_number);
+        etTotalNumber = view.findViewById(R.id.et_total_number);
+        tvResultAmountTotal = view.findViewById(R.id.tv_result_amount_total);
+
+        clickListener();
+        return view;
+    }
+
+    // The clickListener method is used to listen for click events from user
+    private void clickListener() {
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!etStartNumber.getText().toString().isEmpty() && !etEndNumber.getText().toString().isEmpty()) {
+                    startNumber = Integer.parseInt(etStartNumber.getText().toString());
+                    endNumber = Integer.parseInt(etEndNumber.getText().toString());
+                    if(startNumber <= endNumber) {
+                        totalNumber = endNumber - startNumber;
+                        calculateTotalAmount(totalNumber);
+                    }else {
+                        Toast.makeText(view.getContext(),"Số cuối phải lớn hơn số đầu",Toast.LENGTH_SHORT).show();
+                    }
+                }else if (!etTotalNumber.getText().toString().isEmpty()) {
+                    totalNumber = Integer.parseInt(etTotalNumber.getText().toString());
+                    calculateTotalAmount(totalNumber);
+                }else {
+                    Toast.makeText(view.getContext(),"Hãy nhập đủ thông số",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etStartNumber.setText("");
+                etEndNumber.setText("");
+                etTotalNumber.setText("");
+                tvResultAmountTotal.setText("");
+            }
+        });
+    }
+
+    // The calculateTotalAmount method is used to calculate amount water and display result on screen
+    private void calculateTotalAmount(int totalNumber) {
+        int result =0;
+        int amountLevel1 = 6869;
+        int amountLevel2 = 8110;
+        int amountLevel3 = 9969;
+        int amountLevel4 = 18318;
+
+        switch (checkLevel(totalNumber)) {
+            case 1:
+                result = amountLevel1*totalNumber;
+                break;
+            case 2:
+                result = amountLevel1*10 + amountLevel2*(totalNumber-10);
+                break;
+            case 3:
+                result = amountLevel1*10 + amountLevel2*10 + amountLevel3*(totalNumber-20);
+                break;
+            case 4:
+                result = amountLevel1*10 + amountLevel2*10 + amountLevel3*10 + amountLevel4*(totalNumber-30);
+                break;
+        }
+        tvResultAmountTotal.setText(numberFormat.format(result));
+    }
+
+    private int checkLevel(int totalNumber) {
+        int level=0;
+        for(int i=0; i<4; i++) {
+            if(totalNumber > 0) {
+                totalNumber -= 10;
+                level++;
+            }
+        }
+        return level;
     }
 }
--- app/src/main/java/com/example/vietcal/fragment/GrossToNetFragment.java
+++ app/src/main/java/com/example/vietcal/fragment/GrossToNetFragment.java
@@ -1,66 +1,155 @@
 package com.example.vietcal.fragment;
 
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.vietcal.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GrossToNetFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class GrossToNetFragment extends Fragment {
+import java.util.Locale;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class GrossToNetFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    private View view;
+    private String indexLocal = "I";
+    private final Locale sk = Locale.forLanguageTag("sk-SK");
+    NumberFormat numberFormat = NumberFormat.getInstance(sk);
 
     public GrossToNetFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GrossToNetFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GrossToNetFragment newInstance(String param1, String param2) {
-        GrossToNetFragment fragment = new GrossToNetFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gross_to_net, container, false);
+        view = inflater.inflate(R.layout.fragment_gross_to_net, container, false);
+        spinnerLocal();
+        clickListener();
+
+        return view;
+    }
+
+    private void clickListener() {
+        Button btnCalculate = view.findViewById(R.id.btn_calculate);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etSalaryGross = view.findViewById(R.id.et_salary_gross);
+                EditText etDependentPerson = view.findViewById(R.id.et_dependent_person);
+                int salaryGross;
+                int dependentPerson;
+                if(!etSalaryGross.getText().toString().isEmpty()) {
+                    salaryGross = Integer.parseInt(etSalaryGross.getText().toString());
+                }else {
+                    Toast.makeText(view.getContext(),"Chưa nhập lương Gross",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!etDependentPerson.getText().toString().isEmpty()) {
+                    dependentPerson = Integer.parseInt(etDependentPerson.getText().toString());
+                }else {
+                    Toast.makeText(view.getContext(),"Chưa nhập số người phụ thuộc",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                convertGrossToNet(salaryGross, dependentPerson);
+            }
+        });
+    }
+
+    private void spinnerLocal() {
+        Spinner spinner = view.findViewById(R.id.spinner_local);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.index_local, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        indexLocal = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void convertGrossToNet(int salaryGross, int dependentPerson) {
+
+        int incomeBeforeTax = calculateIncomeBeforeTax(salaryGross);
+        int incomeTax = incomeBeforeTax - 11000000 - dependentPerson*4400000;
+        int personalIncomeTax = calculatePersonalIncomeTax(incomeTax, incomeBeforeTax);
+        int salaryNet = incomeBeforeTax-personalIncomeTax;
+
+        TextView tvSalaryNetResult = view.findViewById(R.id.tv_salary_net_result);
+        tvSalaryNetResult.setText(numberFormat.format(salaryNet));
+        Log.e("incomeBeforeTax",numberFormat.format(incomeBeforeTax));
+        Log.e("incomeTax",numberFormat.format(incomeTax));
+        Log.e("personalIncomeTax",numberFormat.format(personalIncomeTax));
+    }
+
+    private int calculateIncomeBeforeTax(int salaryGross) {
+        int incomeBeforeTax = 0;
+        int socialInsurance = (int) (salaryGross*0.08);
+        int healthInsurance = (int) (salaryGross*0.015);
+        int accidentInsurance = (int) (salaryGross*0.01);
+        if(socialInsurance > 2880000) {
+            socialInsurance = 2880000;
+        }
+        if(healthInsurance > 540000) {
+            healthInsurance = 540000;
+        }
+        if (accidentInsurance > 832000) {
+            accidentInsurance = 832000;
+        }
+        incomeBeforeTax = salaryGross - (socialInsurance + healthInsurance + accidentInsurance);
+        return incomeBeforeTax;
+    }
+    private int calculatePersonalIncomeTax(int incomeTax, int incomeBeforeTax) {
+        int personalIncomeTax = 0;
+        int amountRedundant = incomeTax;
+        if(incomeTax > 80000000) {
+//            personalIncomeTax = (incomeBeforeTax*35/100) - 9850000;
+            amountRedundant -= 80000000;
+            personalIncomeTax = 250000 + 500000 + 1200000 + 2800000 + 5000000 + 8400000 + (int)(amountRedundant*0.35);
+            Log.e("Run",numberFormat.format(personalIncomeTax));
+        }else if(incomeTax > 52000000) {
+            amountRedundant -= 52000000;
+            personalIncomeTax = 250000 + 500000 + 1200000 + 2800000 + 5000000 + (int)(amountRedundant*0.3);
+        }else if(incomeTax > 32000000) {
+            amountRedundant -= 32000000;
+            personalIncomeTax = 250000 + 500000 + 1200000 + 2800000 + (int)(amountRedundant*0.25);
+        }else if(incomeTax > 18000000) {
+            amountRedundant -= 18000000;
+            personalIncomeTax = 250000 + 500000 + 1200000 + (int)(amountRedundant*0.2);
+        }else if(incomeTax > 10000000) {
+            amountRedundant -= 10000000;
+            personalIncomeTax = 250000 + 500000 + (int)(amountRedundant*0.15);
+        }else if(incomeTax > 5000000) {
+            amountRedundant -= 5000000;
+            personalIncomeTax = 250000 + (int)(amountRedundant*0.1);
+        }else if(incomeTax > 0){
+            personalIncomeTax = (int)(incomeTax*0.05);
+        }else {
+            personalIncomeTax = 0;
+        }
+        return personalIncomeTax;
     }
 }
